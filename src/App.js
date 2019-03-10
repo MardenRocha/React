b26036df61a7b19364f340848e24dfac86c1c571
MardenRocha <mardenbiel@gmail.com>
2019-03-10 16:13:47 -0300

    Refactoring some classes.

--- src/App.js
+++ src/App.js
@@ -1,161 +1,164 @@
-//The Following code is my baby steps in REACT. 
+//The Following code are my baby steps in REACT.
 
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import Products from './components/Products';
-import NavBar from './components/NavBar';
-import Clients from './components/Clients';
-import {BrowserRouter,Route} from  'react-router-dom';
-
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Products from "./components/Products";
+import NavBar from "./components/NavBar";
+import Clients from "./components/Clients";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
-    this.state={list:[],product:'',count:0,users:[],isLoading:true,modalShow:false,newUser:{}};
+    this.state = {
+      list: [],
+      product: "",
+      count: 0,
+      users: [],
+      isLoading: true,
+      modalShow: false,
+      newUser: {}
+    };
   }
 
-  handleAddList = (id) =>{
-        
+  handleAddList = id => {
     const updateList = this.state.list;
-    updateList[id].count++; 
-    this.setState({list:updateList});  
+    updateList[id].count++;
+    this.setState({ list: updateList });
+  };
 
-  }
-
-  handleSubmitClient = (event) =>{
-
+  handleSubmitClient = event => {
     event.preventDefault();
 
     let user = this.state.newUser;
-    user['id'] = this.state.users.length+1;
+    user["id"] = this.state.users.length + 1;
     let updateUsers = this.state.users;
     updateUsers.push(user);
-    this.setState({users:updateUsers,modalShow:false,newUser:{}});
+    this.setState({ users: updateUsers, modalShow: false, newUser: {} });
+  };
 
-
-  }
-
-  onBlurField = (event) => {
-
+  onBlurField = event => {
     //event.preventDefault();
 
     const field = event.target.name;
-    
+
     let user = this.state.newUser;
 
-    if(field==='company')
-        user[field]={name:event.target.value};
-    else 
-        user[field] = event.target.value;
+    if (field === "company") user[field] = { name: event.target.value };
+    else user[field] = event.target.value;
 
-    this.setState({newUser:user});
+    this.setState({ newUser: user });
+  };
 
-  }
+  handleDeleteList = id => {
+    const updateList = this.state.list.filter(item => item.id !== id);
+    this.setState({ list: updateList });
+  };
 
-  handleDeleteList = (id) => {
-    
-      const updateList = this.state.list.filter(item=>item.id!==id);
-      this.setState({list:updateList});
-
-  }
-
-  handleDeleteUsers = (id) => {
-
-    if(window.confirm('Are you sure?')){
-
-      const updateUsers = this.state.users.filter(item=>item.id!==id);
-      this.setState({users:updateUsers});
-
+  handleDeleteUsers = id => {
+    if (window.confirm("Are you sure?")) {
+      const updateUsers = this.state.users.filter(item => item.id !== id);
+      this.setState({ users: updateUsers });
     }
+  };
 
-  }
-
-  handleRemoveList = (id) => {
-
-     if(this.state.list[id].count>0){
-      
+  handleRemoveList = id => {
+    if (this.state.list[id].count > 0) {
       let updateList = this.state.list;
       updateList[id].count--;
-      this.setState({list:updateList}); 
-      
-    } 
+      this.setState({ list: updateList });
+    }
+  };
 
-  }
-
-  handleSubmitList = (event) => {
-
+  handleSubmitList = event => {
     event.preventDefault();
 
-    if(this.state.product!==''){
-  
-      let countUp = this.state.count+1;
+    if (this.state.product !== "") {
+      let countUp = this.state.count + 1;
       const newProduct = {
-        id:countUp,
-        description:this.state.product,
-        count:0
+        id: countUp,
+        description: this.state.product,
+        count: 0
       };
 
       let updateList = [];
 
-      if(this.state.list.length>0){
-
-        updateList=this.state.list;
+      if (this.state.list.length > 0) {
+        updateList = this.state.list;
         updateList.push(newProduct);
-      
-      }else{
-        
-        updateList=[newProduct];
-      
+      } else {
+        updateList = [newProduct];
       }
 
-      this.setState({list:updateList,product:'',count:countUp});
+      this.setState({ list: updateList, product: "", count: countUp });
     }
+  };
 
+  onChangeProduct = event => {
+    this.setState({ product: event.target.value });
+  };
 
-  }
-
-  onChangeProduct = (event) => {
-
-      this.setState({product:event.target.value});
-
-  }
-
-  setUsers=(props)=>{
-
-    this.setState({users:props.users,isLoading:props.isLoading});
-    
-  }
+  setUsers = props => {
+    this.setState({ users: props.users, isLoading: props.isLoading });
+  };
 
   toggleModal = () => {
-
-      this.setState({modalShow:!this.state.modalShow});
-
-  }
+    this.setState({ modalShow: !this.state.modalShow });
+  };
 
   render() {
     return (
-      
       <BrowserRouter>
         <div>
-          <NavBar/>
-                <Route 
-                  exact path='/' 
-                  render={(props)=><Products add={this.handleAddList} rem={this.handleRemoveList} del={this.handleDeleteList} subm = {this.handleSubmitList} change = {this.onChangeProduct}
-                  list={this.state.list} product={this.state.product} count={this.state.count}/>}
-                />
-                <Route 
-                  path='/products' 
-                  render={(props)=><Products add={this.handleAddList} rem={this.handleRemoveList} del={this.handleDeleteList} subm = {this.handleSubmitList} change = {this.onChangeProduct}
-                  list={this.state.list} product={this.state.product} count={this.state.count}/>}
-                />
-                <Route
-                  path='/clients' 
-                  render={(props)=><Clients users={this.state.users} isLoading = {this.state.isLoading}setUsers={this.setUsers} del={this.handleDeleteUsers} setModal = {this.toggleModal} modalShow = {this.state.modalShow} handleSubmitClient = {this.handleSubmitClient}
-                  onBlurField = {this.onBlurField}/>}
-                />
-            </div>
+          <NavBar />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Products
+                add={this.handleAddList}
+                rem={this.handleRemoveList}
+                del={this.handleDeleteList}
+                subm={this.handleSubmitList}
+                change={this.onChangeProduct}
+                list={this.state.list}
+                product={this.state.product}
+                count={this.state.count}
+              />
+            )}
+          />
+          <Route
+            path="/products"
+            render={props => (
+              <Products
+                add={this.handleAddList}
+                rem={this.handleRemoveList}
+                del={this.handleDeleteList}
+                subm={this.handleSubmitList}
+                change={this.onChangeProduct}
+                list={this.state.list}
+                product={this.state.product}
+                count={this.state.count}
+              />
+            )}
+          />
+          <Route
+            path="/clients"
+            render={props => (
+              <Clients
+                users={this.state.users}
+                isLoading={this.state.isLoading}
+                setUsers={this.setUsers}
+                del={this.handleDeleteUsers}
+                setModal={this.toggleModal}
+                modalShow={this.state.modalShow}
+                handleSubmitClient={this.handleSubmitClient}
+                onBlurField={this.onBlurField}
+              />
+            )}
+          />
+        </div>
       </BrowserRouter>
     );
   }
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,107 +1,117 @@
-import React, { Component } from 'react';
-import ModalNewClient from './ModalNewClient';
+import React, { Component } from "react";
+import ModalNewClient from "./ModalNewClient";
 
 class Clients extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: this.props.users, isLoading: this.props.isLoading };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {users:this.props.users,isLoading:this.props.isLoading};
-    }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res=>res.json())
-            .then(users=>{
-                if(this.state.users.length===0&&this.state.isLoading){
-                    this.props.setUsers({users,isLoading:false});
-                }
-            })//this.setState({users})
-            .catch(e=>console.log(e));
-    }
-
-    usersView(){
-        const {users,isLoading}=this.props;
-        if (users.length>0){//case 'user!=null'
-            return(
-                <div className='container border rounded my-4'>
-                    <div >
-                        <button type="button" onClick = {()=>this.props.setModal()} className="btn btn-success font-weight-bold my-2">
-                            Add Client <span className="badge badge-light font-weight-bold">+</span>
-                            <span className="sr-only font-weight-bold">Add Client</span>
-                        </button>
-
-                        <ModalNewClient show = {this.props.modalShow} onHide={()=>this.props.setModal()}
-                        handleSubmitClient = {this.props.handleSubmitClient} onBlurField = {this.props.onBlurField}/>
-                    </div>                  
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Phone</th>
-                                <th scope="col">Website</th>
-                                <th scope="col">Company</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>{
-                            users.map(user=>
-                                <tr key={user.id}>
-                                    <th scope="row">{user.id}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.phone}</td>
-                                    <td>{user.website}</td>
-                                    <td>{user.company.name}</td>
-                                    <td>
-                                        <button onClick = {()=>this.props.del(user.id)} type="button" className="close" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-                </div>
-            );
-        }else if(isLoading){    
-                return(      
-                    <div className="d-flex justify-content-center">
-                        <div className="spinner-border" role="status">
-                            <span className="sr-only">Loading...</span>
-                        </div>
-                    </div>
-                );
-            }else{
-
-                return(
-
-                    <div className= 'container mt-5'>
-                        <button type="button" onClick={this.openModal} className="btn btn-success font-weight-bold">
-                            Add Client <span className="badge badge-light font-weight-bold">+</span>
-                            <span className="sr-only font-weight-bold">Add Client</span>
-                        </button>                                     
-                        <div class="alert alert-danger font-weight-bold" role="alert">
-                            No records found! :(
-                        </div>
-                  </div>
-
-                );
-
-            }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => {
+        if (this.state.users.length === 0 && this.state.isLoading) {
+          this.props.setUsers({ users, isLoading: false });
         }
-    
-    
+      }) //this.setState({users})
+      .catch(e => console.log(e));
+  }
 
-    render(){
-        return this.usersView();
+  usersView() {
+    const { users, isLoading } = this.props;
+    if (users.length > 0) {
+      //case 'user!=null'
+      return (
+        <div className="container border rounded my-4">
+          <div>
+            <button
+              type="button"
+              onClick={() => this.props.setModal()}
+              className="btn btn-success font-weight-bold my-2"
+            >
+              Add Client{" "}
+              <span className="badge badge-light font-weight-bold">+</span>
+              <span className="sr-only font-weight-bold">Add Client</span>
+            </button>
+
+            <ModalNewClient
+              show={this.props.modalShow}
+              onHide={() => this.props.setModal()}
+              handleSubmitClient={this.props.handleSubmitClient}
+              onBlurField={this.props.onBlurField}
+            />
+          </div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Website</th>
+                <th scope="col">Company</th>
+                <th scope="col" />
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(user => (
+                <tr key={user.id}>
+                  <th scope="row">{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.website}</td>
+                  <td>{user.company.name}</td>
+                  <td>
+                    <button
+                      onClick={() => this.props.del(user.id)}
+                      type="button"
+                      className="close"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else if (isLoading) {
+      return (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container mt-5">
+          <button
+            type="button"
+            onClick={this.openModal}
+            className="btn btn-success font-weight-bold"
+          >
+            Add Client{" "}
+            <span className="badge badge-light font-weight-bold">+</span>
+            <span className="sr-only font-weight-bold">Add Client</span>
+          </button>
+          <div class="alert alert-danger font-weight-bold" role="alert">
+            No records found! :(
+          </div>
+        </div>
+      );
     }
+  }
+
+  render() {
+    return this.usersView();
+  }
 }
-
-
 
 export default Clients;
